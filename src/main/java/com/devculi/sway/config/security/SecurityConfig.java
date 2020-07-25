c120aa9ef3de4b9fe80e35c6aa12843ed1fcab5c
yeti98 <yeti98@github.com>
2020-07-25 21:58:49 +0700

    Base giao diện user

--- src/main/java/com/devculi/sway/config/security/SecurityConfig.java
+++ src/main/java/com/devculi/sway/config/security/SecurityConfig.java
@@ -11,7 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable();
+    http.headers().frameOptions().sameOrigin();
     http.authorizeRequests().anyRequest().permitAll();
   }
 }
--- src/main/java/com/devculi/sway/controller/MainController.java
+++ src/main/java/com/devculi/sway/controller/MainController.java
@@ -1,16 +1,16 @@
 package com.devculi.sway.controller;
 
+import com.devculi.sway.interceptor.attr.annotations.HomePage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
+@HomePage
 public class MainController {
-  static {
-    System.out.println("Scanned");
-  }
 
   @GetMapping
   public String index() {
